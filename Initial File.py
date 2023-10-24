d33c9f0f881200a898d4a6cb28532bdddbaf86a2
kalc1 <kalcocer1@gmail.com>
2023-10-23 19:58:07 -0700

    import first image (Sky.png) into pygame

--- Initial File.py
+++ Initial File.py
@@ -3,10 +3,12 @@ from sys import exit
 
 # pygame setup
 pygame.init()
-screen = pygame.display.set_mode((800,400))
+screen = pygame.display.set_mode((800,400)) # (width, height)
 pygame.display.set_caption('First Game')
 clock = pygame.time.Clock()
 running = True
+
+test_surface = pygame.image.load('C:/Users\kalco\Coding Projects\Python\Pygame-Test\graphics\Sky.png')
 
 while running:
     # poll for events
@@ -15,6 +17,9 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+            
+    screen.blit(test_surface, (0,0))     
+            
     # update everything
     pygame.display.update()
     clock.tick(60)
@@ -23,13 +28,5 @@ while running:
 
 
 
-
-
-
-
-
-
-
-
-
+# This closes the game window once the while-loop above ends when the x button is pressed
 pygame.quit()          
